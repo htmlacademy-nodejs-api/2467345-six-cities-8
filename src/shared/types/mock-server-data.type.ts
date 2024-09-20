67d1219cd8baba44187ca761a94ee3ddd476a8bd
Alexandr <61061837+AlexandrVanchukov@users.noreply.github.com>
2024-09-20 17:07:41 +0300

    Добавил тип MockServerData

---
+++ src/shared/types/mock-server-data.type.ts
@@ -0,0 +1,14 @@
+export type MockServerData = {
+  titles: string[];
+  descriptions: string[];
+  createdDates: string[];
+  cities: string[];
+  previewImages: string[];
+  images: string[];
+  isPremium: string[];
+  isFavorite: string[];
+  types: string[];
+  goods: string[];
+  hostIds: string[];
+  locations: string[];
+};
