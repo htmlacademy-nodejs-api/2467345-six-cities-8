c0f5164cfae62e7baa79dd604e07a6588ada2df1
Alexandr <61061837+AlexandrVanchukov@users.noreply.github.com>
2024-09-25 19:05:44 +0300

    3.20. Добавил перечисление Component

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -4,4 +4,4 @@ export { Offer, OfferType, Goods, City } from './offer.type.js';
 export { User } from './user.type.js';
 
 export { MockServerData } from './mock-server-data.type.js';
-
+export { Component } from './component.enum.js';
