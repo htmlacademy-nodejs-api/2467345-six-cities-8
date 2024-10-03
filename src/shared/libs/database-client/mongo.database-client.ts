501d2bcc7ef3b1c81d7bf826bf9e35e3cfa2705e
Alexandr <61061837+AlexandrVanchukov@users.noreply.github.com>
2024-10-03 09:52:46 +0300

    4.10. Повторное подключение к базе данных.

--- src/shared/libs/database-client/mongo.database-client.ts
+++ src/shared/libs/database-client/mongo.database-client.ts
@@ -1,19 +1,22 @@
 import * as Mongoose from 'mongoose';
 import { inject, injectable } from 'inversify';
+import { setTimeout } from 'node:timers/promises';
 
 import { DatabaseClient } from './database-client.interface.js';
 import { Component } from '../../types/index.js';
 import { Logger } from '../logger/index.js';
 
+const RETRY_COUNT = 5;
+const RETRY_TIMEOUT = 1000;
+
 @injectable()
 export class MongoDatabaseClient implements DatabaseClient {
   private mongoose: typeof Mongoose;
-  private isConnected: boolean;
+  private isConnected: boolean = false;
 
   constructor(
     @inject(Component.Logger) private readonly logger: Logger
   ) {
-    this.isConnected = false;
   }
 
   public isConnectedToDatabase() {
@@ -21,21 +24,33 @@ export class MongoDatabaseClient implements DatabaseClient {
   }
 
   public async connect(uri: string): Promise<void> {
-    if (this.isConnectedToDatabase()) {
+    if (this.isConnected) {
       throw new Error('MongoDB client already connected');
     }
 
     this.logger.info('Trying to connect to MongoDB…');
 
-    this.mongoose = await Mongoose.connect(uri);
-    this.isConnected = true;
+    let attempt = 0;
+    while (attempt < RETRY_COUNT) {
+      try {
+        this.mongoose = await Mongoose.connect(uri);
+        this.isConnected = true;
+        this.logger.info('Database connection established.');
+        return;
+      } catch (error) {
+        attempt++;
+        this.logger.error(`Failed to connect to the database. Attempt ${attempt}`, error as Error);
+        await setTimeout(RETRY_TIMEOUT);
+      }
+    }
 
-    this.logger.info('Database connection established.');
+    throw new Error(`Unable to establish database connection after ${RETRY_COUNT}`);
   }
 
   public async disconnect(): Promise<void> {
-    if (!this.isConnectedToDatabase()) {
-      throw new Error('Not connected to the database');
+    //что-то более мягкое чем ошибка
+    if (!this.isConnected) {
+      console.warn('Not connected to the database');
     }
 
     await this.mongoose.disconnect?.();
