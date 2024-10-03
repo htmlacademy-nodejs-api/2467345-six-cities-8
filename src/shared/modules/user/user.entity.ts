fbe69a68ee97ad41abd8e15860a3b50cb80b0677
Alexandr <61061837+AlexandrVanchukov@users.noreply.github.com>
2024-10-03 10:18:48 +0300

    4.17. Добавит определение модели и схемы. WIP

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,10 +1,23 @@
+import { getModelForClass, prop } from '@typegoose/typegoose';
+
 import { User } from '../../types/index.js';
 import { UserType } from '../../types/user.type.js';
 
 export class UserEntity implements User {
+  @prop({ unique: true, required: true })
   public email: string;
+
+  @prop({ required: true })
   public name: string;
+
+  @prop({ required: false, default: '' })
   public avatarUrl: string | null;
+
+  @prop({ required: true })
   public password: string;
+
+  @prop({ required: true, default: 'default' })
   public type: UserType;
 }
+
+export const UserModel = getModelForClass(UserEntity);
