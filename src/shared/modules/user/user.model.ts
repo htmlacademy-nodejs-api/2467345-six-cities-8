d72328d80a5f944830bc7e73ff26334ea7832f1e
Alexandr <61061837+AlexandrVanchukov@users.noreply.github.com>
2024-10-03 10:13:05 +0300

    4.14. Задействует встроенную валидацию. WIP

--- src/shared/modules/user/user.model.ts
+++ src/shared/modules/user/user.model.ts
@@ -7,12 +7,22 @@ export interface UserDocument extends User, Document {
 }
 
 const userSchema = new Schema({
-  name: String,
   email: {
     type: String,
     unique: true,
+    match: [/^([\w-\\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 'Email is incorrect'],
+    required: true,
   },
-  avatarUrl: String,
+  avatarUrl: {
+    type: String,
+    required: true,
+    minlength: [5, 'Min length for avatar path is 5'],
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: [2, 'Min length for firstname is 2']
+  },
   password: String,
   type: UserType,
 }, { timestamps: true });
