20d1f22278849e1beac39472858703b7cd359014
Alexandr <61061837+AlexandrVanchukov@users.noreply.github.com>
2024-10-03 10:11:31 +0300

    4.13. Добавит уникальность для поля email. WIP

--- src/shared/modules/user/user.model.ts
+++ src/shared/modules/user/user.model.ts
@@ -8,7 +8,10 @@ export interface UserDocument extends User, Document {
 
 const userSchema = new Schema({
   name: String,
-  email: String,
+  email: {
+    type: String,
+    unique: true,
+  },
   avatarUrl: String,
   password: String,
   type: UserType,
